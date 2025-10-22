e80be70efecae25cc62f93c1d738734283cd00c2
Mclaren Senna mp4/6 <100214869+GuilhermeNobrega@users.noreply.github.com>
2025-10-22 16:39:24 -0300

    Create conversãoDataTypes.kt

---
+++ conversãoDataTypes.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+  var intx: Int = 50
+  var floatX = intx.toFloat()
+  println("$floatX")
+}
+//toByte(), toShort(), toInt(), toLong(), toFloat(), toDouble() or toChar():
