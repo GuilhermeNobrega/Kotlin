fd0a72b38a064ac6daa9fc601e7f48c33c40af22
Mclaren Senna mp4/6 <100214869+GuilhermeNobrega@users.noreply.github.com>
2025-10-22 15:56:00 -0300

    Create helloworld.kts

---
+++ helloworld.kts
@@ -0,0 +1,9 @@
+fun calculo(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun main(args: Array<String>) {
+    val valor = calculo(4, 2)  // 'val' define uma variável imutável
+    println("Ola, jogador $valor")
+    //println(calculo(4,2))
+}
