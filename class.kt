91cb8d3254809bb26221a244dff54460fae9401f
Mclaren Senna mp4/6 <100214869+GuilhermeNobrega@users.noreply.github.com>
2025-12-04 16:45:47 -0300

    Update class.kt

--- class.kt
+++ class.kt
@@ -1,18 +1,48 @@
-class Carro(val modelo: String) {
+class Carro(val modelo: String, val ano: Int) {
 
     fun acelerar(): String {
-        return "$modelo esta acelerando!"
+        return "$modelo de $ano esta acelerando!"
     }
 
     fun frear() {
-        println("$modelo esta freiando!")
+        println("$modelo de $ano  esta freiando!")
     }
 }
 
 
 fun main(args: Array<String>){
-  val meuModelo = Carro("BMW")
+  val meuModelo = Carro("BMW",2004)
   println(meuModelo.acelerar())
   meuModelo.frear()
   
+}
+
+VS
+
+import java.util.*;
+
+class Carro {
+    private String modelo;
+    private int ano;
+
+    // Construtor
+    public Carro(String modelo, int ano) {
+        this.modelo = modelo; // igual ao Kotlin fazendo automaticamente
+        this.ano = ano;
+    }
+
+    public String acelerando() {
+        return modelo + " está acelerando";
+    }
+    public void freiando(){
+      System.out.println( modelo + "esta freiando");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Carro meuModelo = new Carro("BMW", 2022);
+        System.out.println(meuModelo.acelerando());
+    }
 }
